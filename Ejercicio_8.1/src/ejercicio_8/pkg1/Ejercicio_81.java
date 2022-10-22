6b65595b7e3cd47372a9597d9a5117ecce63ca04
Kevin Guio <64978456+KevinGuio@users.noreply.github.com>
2022-10-22 12:36:45 -0500

    Add files via upload

---
+++ Ejercicio_8.1/src/ejercicio_8/pkg1/Ejercicio_81.java
@@ -0,0 +1,10 @@
+package ejercicio_8.pkg1;
+
+public class Ejercicio_81 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; // Define la ventana principal
+        miVentanaPrincipal= new VentanaPrincipal(); //Crea la ventana principal
+        miVentanaPrincipal.setVisible(true); // Establece la ventana como visible
+        }   
+}
